4817411cc55c052e253439386f386af1670a4f8f
Abhay Kumar Mehtre <33192229+abhaymehtre@users.noreply.github.com>
2023-01-08 15:57:22 +0530

    Update ipfs.js

--- client/src/utils/ipfs.js
+++ client/src/utils/ipfs.js
@@ -1,3 +1,16 @@
 const IPFS = require('ipfs-api');
-const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+const ipfs = IPFS('localhost', '5001');
+
+// ipfs.id(function(err, res) {
+//     if (err) throw err
+//     console.log('Connected to IPFS node!', res.id, res.agentVersion, res.protocolVersion);
+//     });
 export default ipfs;
+
+
+
+
+ // CORS disable for IPFS transfer data.
+// ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
+// ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials "[\"true\"]"
+// ipfs daemon
